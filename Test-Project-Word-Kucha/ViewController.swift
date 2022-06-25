a99a6416d253438cabc20abb55dbef4b783879eb
Pavel <pavel.818@yandex.ru>
2022-06-25 10:01:01 +0400

    Initial Commit

---
+++ Test-Project-Word-Kucha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-Project-Word-Kucha
+//
+//  Created by Pavel Абрамов on 25.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
